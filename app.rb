87e72a1c9ffcb618db13e6ff4fade969ee050350
Phil Nash <philnash@gmail.com>
2016-05-03 15:47:03 +0100

    Adds Twilio gem and controls lights by SMS and voice

--- app.rb
+++ app.rb
@@ -1,6 +1,52 @@
+sms_number = "+447481342184"
+voice_number = "+441473379529"
+
+use Rack::TwilioWebhookAuthentication, ENV['AUTH_TOKEN'], '/messages'
+
+COLOURS = {
+  red: 0,
+  green: 26000,
+  blue: 42000,
+  yellow: 10000
+}
+
+NUMBERS = {
+  1 => :red,
+  2 => :green,
+  3 => :blue,
+  4 => :yellow
+}
+
 get "/" do
   lights = client.lights
   erb(:index, :locals => { :lights => lights })
+end
+
+post "/messages" do
+  light = find_light(client.lights, "4")
+  hue = params["Body"].to_i
+  light.hue = hue
+  "<Response>
+    <Message>The light is set to #{hue}!</Message>
+  </Response>"
+end
+
+post "/voice" do
+  if params["Digits"]
+    colour = NUMBERS[params["Digits"].to_i]
+    welcome = "Thanks, light turned to #{colour}."
+    light = find_light(client.lights, "4")
+    light.hue = COLOURS[colour]
+  else
+    welcome = "Welcome to dial a colour. "
+  end
+  "
+  <Response>
+    <Gather action='/voice' numDigits='1'>
+      <Say loop='0' voice='alice'>#{welcome} Dial one for red. Dial two for green. Dial three for blue. Dial four for yellow...</Say>
+    </Gather>
+  </Response>
+  "
 end
 
 post "/toggle/:id" do
@@ -21,8 +67,7 @@ post "/toggle/:id" do
 end
 
 post "/colour/:id" do
-  lights = client.lights
-  light = lights.detect { |l| l.id == params[:id] }
+  light = find_light(client.lights, params[:id])
   light.hue = params["hue"].to_i
   { :light => params[:id], :hue => params["hue"] }.to_json
 end
@@ -31,7 +76,10 @@ def client
   @client ||= Hue::Client.new
 end
 
-
+def find_light(lights, id)
+  lights = client.lights
+  light = lights.detect { |l| l.id == id }
+end
 
 
 

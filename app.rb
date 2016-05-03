74861eea3fa8052e905cb4147f6cf8509b3b8a59
Phil Nash <philnash@gmail.com>
2016-05-03 15:11:26 +0100

    Adds controls for the lights to the web page

--- app.rb
+++ app.rb
@@ -1,6 +1,39 @@
-require "bundler"
-Bundler.require
-
 get "/" do
-  erb(:index)
+  lights = client.lights
+  erb(:index, :locals => { :lights => lights })
 end
+
+post "/toggle/:id" do
+  lights = client.lights
+  light = lights.detect { |l| l.id == params[:id] }
+  if light.on?
+    light.off!
+    status = "off"
+  else
+    light.on!
+    status = "on"
+  end
+  if request.xhr?
+    { :light => params[:id], :status => status }.to_json
+  else
+    redirect("/")
+  end
+end
+
+post "/colour/:id" do
+  lights = client.lights
+  light = lights.detect { |l| l.id == params[:id] }
+  light.hue = params["hue"].to_i
+  { :light => params[:id], :hue => params["hue"] }.to_json
+end
+
+def client
+  @client ||= Hue::Client.new
+end
+
+
+
+
+
+
+
